75b2cc0434eedcfe1487e69ea116646b4a707199
Ujjwal Maity <ujjwalmaity98@gmail.com>
2024-11-08 07:09:34 +0530

    Interface

--- advance.js
+++ advance.js
@@ -1,8 +1,10 @@
 // Class
 // Non static variable
 // Constructor
+// Static variable, Static method
 var A = /** @class */ (function () {
     function A(id, city) {
+        this.country = "IN";
         this.id = id;
         this.city = city;
     }
@@ -14,16 +16,37 @@ var A = /** @class */ (function () {
         // return city; // Error
         return this.city;
     };
+    A.sTest = function () {
+        console.log('class A static sTest() method');
+    };
+    A.country = "India";
     return A;
 }());
-// var a1 = new A(101, "Kolkata");
+var a1 = new A(101, "Kolkata");
 // console.log(a1.id, a1.city);
 // a1.setCity("Bangalore");
 // console.log(this.city); // undefined
 // console.log(a1.city);
 // console.log(a1.getCity());
-// var a2 = {
-//     "id": 201,
-//     "city": "Chennai"
-// };
-// console.log(a2.city);
+// console.log(A.country);
+// console.log(a1.country);
+// A.sTest();
+var a2 = {
+    "id": 201,
+    "city": "Chennai"
+};
+var B = /** @class */ (function () {
+    function B() {
+    }
+    B.prototype.testX = function () {
+        console.log("class B - testX() method ".concat(this.city));
+    };
+    B.prototype.testY = function () {
+        console.log("class B - testY() method");
+    };
+    return B;
+}());
+// var b1 = new B();
+// b1.city = 'Kol';
+// b1.testX();
+// b1.testY();
--- advance.ts
+++ advance.ts
@@ -1,10 +1,13 @@
 // Class
 // Non static variable
 // Constructor
+// Static variable, Static method
 class A {
     // var id: number; // Error
     id: number;
     city: string;
+    static country: string = "India";
+    country = "IN";
 
     constructor(id: number, city: string) {
         this.id = id;
@@ -20,16 +23,53 @@ class A {
         // return city; // Error
         return this.city;
     }
+
+    static sTest(): void {
+        console.log('class A static sTest() method');
+    }
 }
 
-// var a1 = new A(101, "Kolkata");
+var a1 = new A(101, "Kolkata");
 // console.log(a1.id, a1.city);
 // a1.setCity("Bangalore");
 // console.log(this.city); // undefined
 // console.log(a1.city);
 // console.log(a1.getCity());
-// var a2 = {
-//     "id": 201,
-//     "city": "Chennai"
-// };
+// console.log(A.country);
+// console.log(a1.country);
+// A.sTest();
+
+var a2 = {
+    "id": 201,
+    "city": "Chennai"
+};
 // console.log(a2.city);
+
+// Interface
+interface X {
+    // var city: string; // Error
+    city: string;
+
+    // function testX(): void; // Error
+    testX(): void;
+}
+interface Y {
+    testY(): void;
+}
+
+class B implements X, Y {
+    city: string;
+
+    testX(): void {
+        console.log(`class B - testX() method ${this.city}`);
+    }
+
+    testY(): void {
+        console.log(`class B - testY() method`);
+    }
+}
+
+// var b1 = new B();
+// b1.city = 'Kol';
+// b1.testX();
+// b1.testY();
